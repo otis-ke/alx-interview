3ec1378501b442bbebe8628a7220ae029141478b
Vincent Otis <110315228+otis-ke@users.noreply.github.com>
2024-07-25 14:24:17 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,6 @@
 0. Pascal's Triangle
 """
 
-
 def pascal_triangle(n):
     """Create a function def pascal_triangle(n): that returns a list of lists
     of integers representing the Pascal’s triangle of n
